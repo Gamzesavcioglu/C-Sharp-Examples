cda9d035d563c5e053193a96b78c08b7f260b157
Gamzesavcioglu <46785635+Gamzesavcioglu@users.noreply.github.com>
2021-01-23 22:22:08 +0300

    Add files via upload

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using Dictionary;
+using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> informations = new MyDictionary<int, string>();
+            informations.Add(12, "Ahmet");
+            informations.Add(41, "Ali");
+            informations.Add(20, "Ayşe");
+            informations.Add(11, "Sırma");
+            informations.List();
+        }
+    }
+}
